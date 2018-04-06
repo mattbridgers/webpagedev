a617bd69bbf7e4177814620a83e5b7c07967bf15
Micah Cooper <mc@micahcooper.com>
2018-04-06 10:20:27 -0400

    Update mattswebsite.js

--- mattswebsite.js
+++ mattswebsite.js
@@ -1,4 +1,13 @@
-function myFunction()
+console.log('loading external javascript');
+
+
+function loadApplication()
 {
-  console.log("hellooooo");
+	console.log('starting javascript functions');
+	var1 = document.getElementById('button1');
+}
+
+function myFunction(firstName, lastName)
+{
+	console.log("hellooooo, i am logging to the console");
 }
